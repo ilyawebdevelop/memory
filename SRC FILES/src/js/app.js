a75c4e1cf15e2e31fb09d65bb412e1c022ef767a
ilyawebdevelop <ilia966@tut.by>
2022-04-21 01:05:33 +0300

    responsive 1 window

--- SRC FILES/src/js/app.js
+++ SRC FILES/src/js/app.js
@@ -1,5 +1,5 @@
 import * as flsFunctions from "./modules/functions.js";
-// import './components.js';
+import './components.js';
 
 flsFunctions.isWebp();
 
@@ -12,7 +12,7 @@ Swiper.use([Navigation, Pagination, Autoplay, Mousewheel ]);
 const memorySlider = document.querySelector('.memory_slider');
 let mySwiperMemory = new Swiper(memorySlider, { 	
   slidesPerView: 4,	
-  spaceBetween: 30,  
+  spaceBetween: 30, 	
   loop: true,  
   pagination: {
 		el: '.swiper-pagination',
@@ -26,17 +26,71 @@ let mySwiperMemory = new Swiper(memorySlider, {
 	breakpoints: {  
 		0: {
 			slidesPerView: 1,	
+			centeredSlides: true,
 		},  
 		576: {
-			slidesPerView: 1,	
+			slidesPerView: 2,	
+			centeredSlides: true,
 		},  
 		768: {
-			slidesPerView: 1,	
+			slidesPerView: 3,
+			centeredSlides: true,	
 		},     
-		992: {		
+		992: {	
+			slidesPerView: 3,	
+			centeredSlides: true,
 		},
 		1200: {		
-		
+			centeredSlides: true,
+		},
+		1500: {		
+			
 		}
 	}
+});
+
+// Burger
+const btnMenu = document.querySelector('#toggle');
+const menu = document.querySelector('.header__nav-list');
+const bodyEl = document.querySelector('body');
+let navItemAll = document.querySelectorAll('.header__nav-item');
+
+
+const toggleMenu = function() {
+	menu.classList.toggle('open');	
+}
+const toggleBurger = function() {
+	btnMenu.classList.toggle('active');
+}
+const bodyOverflow = function() {
+	bodyEl.classList.toggle('hidden');
+}
+btnMenu.addEventListener('click', function(e) {
+	e.stopPropagation();
+	toggleMenu();		
+	toggleBurger();
+	bodyOverflow();
+});	
+ navItemAll.forEach((el) => {		
+	el.addEventListener("click", function () {
+		if (menu.classList.contains("open")) {			
+			toggleMenu();
+			toggleBurger();
+			bodyOverflow();
+		}				
+	});	
+})
+
+// Menu show
+document.addEventListener('click', function(e) {
+    const target = e.target;
+    const its_menu = target == menu || menu.contains(target);		
+		
+    const menu_is_active = menu.classList.contains('open');	
+	
+    if (!its_menu && menu_is_active) {
+	toggleMenu();
+	toggleBurger();
+	bodyOverflow();
+    }		
 });
